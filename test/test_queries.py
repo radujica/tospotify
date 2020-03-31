71a726781c1c4b5e6428c0cba0ce64cae95d2873
Radu <radu.jica@gmail.com>
2020-03-31 14:48:20 +0200

    implement wildcard and typing

--- test/test_queries.py
+++ test/test_queries.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tospotify.queries import QueryArtistTitle, QueryMultipleArtists, QueryTitle
+from tospotify.queries import QueryArtistTitle, QueryMultipleArtists, QueryTitle, QueryWildcard
 
 
 @pytest.mark.parametrize("artist,title,query_class,expected", [
@@ -9,7 +9,9 @@ from tospotify.queries import QueryArtistTitle, QueryMultipleArtists, QueryTitle
     ('Sting;The Police', 'Shape of my heart', QueryMultipleArtists,
      ['artist:"Sting" AND track:"Shape of my heart"', 'artist:"The Police" AND track:"Shape of my heart"']),
     ('Sting', 'Shape of my heart', QueryTitle,
-     ['track:"Shape of my heart"'])
+     ['track:"Shape of my heart"']),
+    ('Sting', 'Shape of my heart', QueryWildcard,
+     ['"Sting Shape of my heart"'])
 ])
 def test_query_compile(artist, title, query_class, expected):
     actual = query_class(artist, title).compile()
--- tospotify/queries.py
+++ tospotify/queries.py
@@ -1,30 +1,35 @@
 import abc
+from typing import List
 
 
 class Query(abc.ABC):
-    def __init__(self, artist, title):
+    def __init__(self, artist: str, title: str) -> None:
         self.artist = artist
         self.title = title
 
-    def makes_sense(self):
+    def makes_sense(self) -> bool:
         """check if this query makes sense; returns bool"""
         raise NotImplementedError
 
-    def compile(self):
+    def compile(self) -> List[str]:
         """Return list of queries"""
         raise NotImplementedError
 
     @staticmethod
+    def _surround_with_quotes(s: str) -> str:
+        return '\"' + s + '\"'
+
+    @staticmethod
     def _prepare_search_query_component(s: str, query_type: str) -> str:
         # need to escape the quotes else requests will url-encode them
-        return query_type + ':' + '\"' + s + '\"'
+        return query_type + ':' + Query._surround_with_quotes(s)
 
 
 class QueryArtistTitle(Query):
-    def makes_sense(self):
+    def makes_sense(self) -> bool:
         return True
 
-    def compile(self):
+    def compile(self) -> List[str]:
         artist = Query._prepare_search_query_component(self.artist, 'artist')
         title = Query._prepare_search_query_component(self.title, 'track')
 
@@ -34,20 +39,28 @@ class QueryArtistTitle(Query):
 
 
 class QueryTitle(Query):
-    def makes_sense(self):
+    def makes_sense(self) -> bool:
         return True
 
-    def compile(self):
+    def compile(self) -> List[str]:
         query = Query._prepare_search_query_component(self.title, 'track')
 
         return [query]
 
 
+class QueryWildcard(Query):
+    def makes_sense(self) -> bool:
+        return True
+
+    def compile(self) -> List[str]:
+        return [Query._surround_with_quotes(self.artist + ' ' + self.title)]
+
+
 class QueryMultipleArtists(Query):
-    def makes_sense(self):
+    def makes_sense(self) -> bool:
         return ';' in self.artist
 
-    def compile(self):
+    def compile(self) -> List[str]:
         artists = self.artist.split(';')
         queries = []
         for artist in artists:
@@ -59,5 +72,5 @@ class QueryMultipleArtists(Query):
 
 # in reversed order, so queries will be tried from right to left
 # first query always tried
-ADDITIONAL_QUERIES = [QueryTitle, QueryMultipleArtists]
+ADDITIONAL_QUERIES = [QueryWildcard, QueryTitle, QueryMultipleArtists]
 DEFAULT_QUERY = QueryArtistTitle
