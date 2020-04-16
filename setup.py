49ffa6a754b7c765b2b9fe2afb72acc30b22a798
Radu <radu.jica@gmail.com>
2020-04-16 16:47:38 +0200

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='tospotify',
-    version='0.1.3',
+    version='0.2',
     description='Create/update a Spotify playlist from a local m3u playlist',
     url='https://github.com/radujica/tospotify',
     author='Radu Jica',
